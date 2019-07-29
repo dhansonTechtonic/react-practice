c7624f107fba9f7c502b293e7df8f450fcaef5b5
Delaney Hanson <delaney.hanson@techtonic.com>
2019-07-29 09:57:24 -0600

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import DisplayPage from './Components/DisplayPage'
-import WorldPage from './Components/WorldPage'
+import InfoPage from './Components/InfoPage'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <Route exact path='/' component={DisplayPage}/>
-      <Route path='/homeworld/' component={WorldPage} />
+      <Route path='/homeworld/' component={InfoPage} />
     </Router>
   );
 }
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 import './SearchBar.css'
-import spyro from '../Assets/Legend_Spyro.webp'
-import DropDown from './DropDown'
 
 class SearchBar extends Component {
     constructor(props){
@@ -10,9 +8,6 @@ class SearchBar extends Component {
     render() {
         return (
             <div className="search-bar">
-                <img src={spyro} alt="spyro the dragon" className="spyro"></img>
-                <h1>Spyro-pedia</h1>
-                <DropDown toggleDisplay={this.props.toggleDisplay} selected={this.props.selected}/>
             </div>
         )
     }
